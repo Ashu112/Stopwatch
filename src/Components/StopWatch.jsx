5bf8f27c7001c98740aae0efe9be518c64dff7e0
Ashu112 <kashu2611@gmail.com>
2024-06-25 22:34:23 +0530

    conditional rendering

--- src/Components/StopWatch.jsx
+++ src/Components/StopWatch.jsx
@@ -1,22 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./StopWatch.css";
 
 const StopWatch = () => {
   const [time, setTime] = useState(0);
   const [start, setStart] = useState(true);
-  const sec = 0;
+
+  const milliseconds = time % 100;
+
+  useEffect(() => {
+    if (start) {
+      setTimeout(() => {
+        setTime(time + 1);
+      }, 10);
+    }
+  }, [start, time]);
+
   return (
     <div className="stopwatch-container">
       <div className="time-container">
         <div className="time-main">
-          {sec} : {time}
+          {} : {milliseconds}
         </div>
       </div>
       <div className="stopwatch-btn">
         {start ? (
-          <button className="btn">Start</button>
+          <button
+            className="btn"
+            onClick={() => {
+              setStart(false);
+            }}
+          >
+            Start
+          </button>
         ) : (
-          <button className="btn">Stop</button>
+          <button
+            className="btn"
+            onClick={() => {
+              setStart(true);
+            }}
+          >
+            Stop
+          </button>
         )}
 
         <button className="btn">Reset</button>
